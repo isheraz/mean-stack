9f3c8f00ce666e031cd375f6f64aff1aa4b7154c
Salman-IZ <salman.kamran@invozone.com>
2020-12-21 18:15:10 +0500

    Postgres DB Connection with ENV

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const userRoutes = require('./routes/userRoutes');
 
+const porject_env = require('dotenv').config();
+const { Pool } = require('pg');
+const constants = require('./constants');
 const app = express();
 
 // using userRoutes
 app.use(userRoutes);
 
+const pool = new Pool({
+  user: constants.DB_USERNAME,
+  host: constants.PG_HOST,
+  database: constants.DATABASE,
+  password: constants.DB_PASSWORD,
+  port: constants.PG_PORT,
+});
+pool.query('SELECT NOW()', (err, res) => {
+  console.log(err, res);
+  pool.end();
+});
+
 app.listen(6000, () => {
-    console.log('server is up and running!');
-  });
+  console.log('server is up and running!');
+});
