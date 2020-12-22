2d71171f332920e018942828043c076cb9e7add7
Muzammal-invozone <muzammil.ikram@invozone.com>
2020-12-22 10:44:05 +0500

    working on blogging/newsletter CRUD validation and error messages.

---
+++ blog.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const bodyParser = require('body-parser');
+
+const app     = express();
+const routes  = require('./routes/blog');
+app.use(express.urlencoded({extended: true})); 
+app.use(express.json());  
+ 
+const port    = process.env.PORT || 6000;
+
+require('dotenv').config()
+app.use('/blog', routes);
+
+app.listen(port, () => {
+ console.log(`Server running on port ${port}`);
+});
