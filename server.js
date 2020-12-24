3985409a9f3a5d217b66b8774ad9a81c38e0c85e
ismail raza <ismail.raza7292@gmail.com>
2020-12-24 16:00:29 +0500

    added cors in server

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express= require ('express');
 const bodyParser= require ('body-parser');
 const routes= require('./routes');
+const cors= require('cors');
 const app= express();
 require ('dotenv').config();
 
+app.use(cors());
 app.use(express.json());
 app.use('/', routes)
 
