13ba14d132250a0fb9b0b9332f4ee061245090e7
rohail411 <rohailbutt411@gmail.com>
2020-12-24 15:26:15 +0500

    task completed

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require('express');
-const userRoutes = require('./routes/userRoutes');
+const dotenv = require('dotenv');
+dotenv.config()
+const app  = express();
+app.use(function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+	res.header('Access-Control-Allow-Headers', 'Authorization, Content-Type Cache-Control');
+	res.header('Cache-Control', 'max-age=0');
+	next();
+});
 
-const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-// using userRoutes
-app.use(userRoutes);
+const eventRoute = require('./routes/eventRoute')(express.Router())
+app.use('/api',eventRoute);
 
-app.listen(6000, () => {
-    console.log('server is up and running!');
-  });
+
+
+app.listen(process.env.PORT,()=>console.log('Server is Listening on Port :3000'));
