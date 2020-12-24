ef2a44480f056dbc73010d45f4af54183e42226d
rohail411 <rohailbutt411@gmail.com>
2020-12-24 18:34:07 +0500

    updated

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -5,8 +5,7 @@ const createEvent = async (req,res)=>{
         const event = await Event.create(req.body)
         return res.status(200).json({event})
     } catch (error) {
-        console.log(error)
-        return res.status(500).json({error})
+        return res.status(500).json({error:'Something Went Wrong Please Try Again'})
     }
 }
 
@@ -15,7 +14,8 @@ const getAllEvents = async (req,res) => {
         const events = await Event.findAll();
         return res.status(200).json({events})
     } catch (error) {
-        return res.status(500).json({error})
+        return res.status(500).json({error:'Something Went Wrong Please Try Again'})
+
     }
 }
 
@@ -25,9 +25,10 @@ const getEventById = async (req,res) => {
 
         const event = await Event.findOne({where:{id:id}});
         if(event) return res.status(200).json({event});
-        throw new Error('Event Id not found');
+        return res.status(404).json({error:'Event with this id not found'}); 
     } catch (error) {
-        return res.status(500).json({error});
+        return res.status(500).json({error:'Something Went Wrong Please Try Again'})
+
     }
 }
 
@@ -40,9 +41,11 @@ const updateEvent = async (req,res)=>{
         if(update){
             const updatedEvent = await Event.findOne({where: {id:id}});
             return res.status(201).json({updatedEvent});
-        }else throw new Error('Something went wrong');
+        }
+         return res.status(404).json({error:'Event with this id not found'}); 
     } catch (error) {
-        return res.status(500).json({error});        
+        return res.status(500).json({error:'Something Went Wrong Please Try Again'})
+        
     }
 }
 
@@ -51,9 +54,10 @@ const deleteEvent = async (req,res)=>{
         const {id} = req.params;
         const deleted = await Event.destroy({where: {id:id}});
         if(deleted) return res.status(200).json({message:'Event Deleted Successfully'});
-        throw new Error('Something went wrong');
+        return res.status(404).json({ error:'Event with this id not found'});
     } catch (error) {
-        return res.status(500).json({error});
+        return res.status(500).json({error:'Something Went Wrong Please Try Again'})
+
     }
 }
 
