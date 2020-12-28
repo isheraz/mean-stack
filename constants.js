7d0ea0cbadf6c9a14c27d47293c82916fbcf9240
rohail411 <rohailbutt411@gmail.com>
2020-12-28 15:27:31 +0500

    validation added

--- constants.js
+++ constants.js
@@ -1,14 +1,15 @@
+require('dotenv').config();
 
 const PG_HOST = process.env.PG_HOST || '127.0.0.1';
 const PG_PORT = process.env.PG_PORT || 45775;
-const DB_USERNAME = process.env.DB_USERNAME || admin;
-const DB_PASSWORD = process.env.DB_PASSWORD || admin;
-const DATABASE = process.env.DATABASE || js_cms;
+const DB_USERNAME = process.env.DB_USERNAME || 'admin';
+const DB_PASSWORD = process.env.DB_PASSWORD || 'admin';
+const DATABASE = process.env.DATABASE || 'js_cms';
 
-module.exports={
-    PG_HOST,
-    PG_PORT,
-    DB_USERNAME,
-    DB_PASSWORD,
-    DATABASE
-  }
+module.exports = {
+  PG_HOST,
+  PG_PORT,
+  DB_USERNAME,
+  DB_PASSWORD,
+  DATABASE,
+};
