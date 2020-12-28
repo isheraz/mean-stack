db7d4fcf9e0c49d7e0ae7aeb6957154c088cb697
ismail raza <ismail.raza7292@gmail.com>
2020-12-28 18:40:29 +0500

    muzamil brach is merged

--- config/connection.js
+++
@@ -1,5 +0,0 @@
-const envConfigs= require('./config');
-const Sequelize= require('sequelize')
-const env = process.env.NODE_ENV || "development";
-const config = envConfigs[env];
-module.exports= new Sequelize(config.url, config);
