013d2ef74d54c63d0c17b5974c035c69f34249c4
rohail411 <rohailbutt411@gmail.com>
2020-12-28 15:27:31 +0500

    validation added

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -1,18 +1,18 @@
+const { validationResult } = require('express-validator');
 const { Event } = require('../models');
-const eventValidator = require('../validations/eventvalidator');
 
 const createEvent = async (req, res) => {
   try {
-    const errors = eventValidator(req.body);
-    if (errors) return res.status(404).json({ error: errors });
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) return res.status(404).json({ error: errors.array() });
     const event = await Event.create(req.body);
     return res.status(200).json({ event });
   } catch (error) {
-    return res.status(500).json({ error: 'Something Went Wrong Please Try Again' });
+    return res.status(500).json({ error });
   }
 };
 
-const getAllEvents = async (req, res) => {
+const getAllEvents = async (_req, res) => {
   try {
     const events = await Event.findAll();
     if (events.length > 0) {
@@ -27,6 +27,7 @@ const getAllEvents = async (req, res) => {
 const getEventById = async (req, res) => {
   try {
     const { id } = req.params;
+    if (!id) return res.status(404).json({ error: 'id must be number' });
     const event = await Event.findOne({ where: { id } });
     if (event) return res.status(200).json({ event });
     return res.status(404).json({ error: 'Event with this id not found' });
@@ -38,6 +39,7 @@ const getEventById = async (req, res) => {
 const updateEvent = async (req, res) => {
   try {
     const { id } = req.params;
+    if (!id) return res.status(404).json({ error: 'id must be number' });
     const [update] = await Event.update(req.body, {
       where: { id },
     });
@@ -54,6 +56,7 @@ const updateEvent = async (req, res) => {
 const deleteEvent = async (req, res) => {
   try {
     const { id } = req.params;
+    if (!id) return res.status(404).json({ error: 'id must be number' });
     const deleted = await Event.destroy({ where: { id } });
     if (deleted) return res.status(200).json({ message: 'Event Deleted Successfully' });
     return res.status(404).json({ error: 'Event with this id not found' });
--- routes/eventRoute.js
+++ routes/eventRoute.js
@@ -1,9 +1,11 @@
+const { body } = require('express-validator');
 const eventController = require('../controllers/eventController');
-module.exports = (router)=>{
-    router.post('/createEvent',eventController.createEvent)
-    router.get('/getEvent/:id',eventController.getEventById)
-    router.get('/getAllEvents',eventController.getAllEvents)
-    router.put('/updateEvent/:id',eventController.updateEvent)
-    router.delete('/deleteEvent/:id',eventController.deleteEvent)
-    return router;
-}
+
+module.exports = (router) => {
+  router.post('/createEvent', [body('name').notEmpty(), body('venue').notEmpty(), body('date').notEmpty()], eventController.createEvent);
+  router.get('/getEvent/:id', eventController.getEventById);
+  router.get('/getAllEvents', eventController.getAllEvents);
+  router.put('/updateEvent/:id', eventController.updateEvent);
+  router.delete('/deleteEvent/:id', eventController.deleteEvent);
+  return router;
+};
