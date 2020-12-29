d2d57c772e2a4c216618c8df5de582b1db0936d0
rohail411 <rohailbutt411@gmail.com>
2020-12-29 13:51:56 +0500

    bugs fixed

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-// const { Pool } = require('pg');
 require('dotenv').config();
-
-// const configuration = require('./constants');
 
 const app = express();
 const role = require('./routes/role');
 const permission = require('./routes/permission');
 const userRoutes = require('./routes/user');
 const eventRoute = require('./routes/eventRoute')(express.Router());
-
-// const {checkRole } = require('./basicAuth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -20,16 +15,8 @@ const port = process.env.PORT || 6000;
 // Define Routes
 app.use('/role', role);
 app.use('/permission', permission);
-app.use('/event',eventRoute);
+app.use('/event', eventRoute);
 app.use(userRoutes);
-
-// const pool = new Pool(configuration.module.local);
-
-// pool.query('SELECT NOW()', (err, res) => {
-//   console.log(err, res);
-//   pool.end();
-// });
-
 
 app.listen(port, () => {
   console.log(`Server running on port localhost:${port}`);
