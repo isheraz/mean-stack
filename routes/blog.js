f9f9f4e07776a1328eb4e18f2bfde21fc5d658df
Muzammal-invozone <muzammil.ikram@invozone.com>
2020-12-30 15:47:05 +0500

    seprate routes to controllers

--- routes/blog.js
+++ routes/blog.js
@@ -1,8 +1,6 @@
-const routes = require('express').Router();
-const { body, validationResult } = require('express-validator');
-
-const blogModel = require('../models').Blog;
-const { Comment } = require('../models');
+const router = require('express').Router();
+const { body } = require('express-validator');
+const blogController = require('../controllers/blog.controller');
 
 const validate = (method) => {
   if (method !== 'Blog') return [body('parameters').notFound()];
@@ -15,92 +13,10 @@ const validate = (method) => {
   ];
 };
 
-const SuccessStatusCode = blogModel.MESSAGE.success.statusCode;
-const SuccessUPDATEMESSAGE = blogModel.MESSAGE.success.message.update;
-const SuccessDELETEMESSAGE = blogModel.MESSAGE.success.message.delete;
-const ERRORStatusCode = blogModel.MESSAGE.error.statusCode;
-const ERRORMESSAGE = blogModel.MESSAGE.error.message;
-const INVALIDSTATUSCODE = blogModel.MESSAGE.invalidData.statusCode;
-const NOTFOUNDSTATUSCODE = blogModel.MESSAGE.notFound.statusCode;
-const NOTFOUNDMESSAGE = blogModel.MESSAGE.notFound.message;
+router.get('/get', blogController.Blogs);
+router.get('/:id', blogController.getBlog);
+router.post('/create', validate('Blog'), blogController.saveBlog);
+router.put('/update/:id', validate('Blog'), blogController.update);
+router.delete('/delete/:id', blogController.delete);
 
-routes.get('/', async (_req, res) => {
-  try {
-    const blogs = await blogModel.findAll({
-      include: {
-        model: Comment,
-        as: 'Comment',
-      },
-    });
-    res.status(SuccessStatusCode).json({ data: blogs });
-  } catch (err) {
-    console.log(err);
-    res.status(ERRORStatusCode).json({ error: err.message });
-  }
-});
-
-routes.post('/create', validate('Blog'), async (req, res) => {
-  try {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      res.status(INVALIDSTATUSCODE).json({ errors: errors.array() });
-      return;
-    }
-
-    const newBlog = await blogModel.create({
-      title: req.body.title,
-      description: req.body.description,
-      userId: req.body.userId,
-      status: req.body.status,
-    });
-    res.json({ data: newBlog });
-  } catch (err) {
-    res.json({ data: ERRORMESSAGE });
-  }
-});
-
-routes.get('/:id', async (req, res) => {
-  const blog = await blogModel.findOne({ where: { id: req.params.id } });
-  res.status(SuccessStatusCode).json({ data: blog });
-});
-
-routes.put('/update/:id', validate('Blog'), async (req, res) => {
-  try {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      res.status(INVALIDSTATUSCODE).json({ errors: errors.array() });
-      return;
-    }
-    await blogModel.update(
-      {
-        title: req.body.title,
-        description: req.body.description,
-        userId: req.body.userId,
-        status: req.body.status,
-      },
-      { where: { id: req.params.id } }
-    );
-    res.status(SuccessStatusCode).json({ data: SuccessUPDATEMESSAGE });
-  } catch (err) {
-    res.json({ data: ERRORMESSAGE });
-  }
-});
-
-routes.delete('/delete/:id', async (req, res) => {
-  try {
-    const blog = await blogModel.findOne({ where: { id: req.params.id } });
-    if (!blog) {
-      res.status(NOTFOUNDSTATUSCODE).json({ data: NOTFOUNDMESSAGE });
-    }
-    await blogModel.destroy({
-      where: {
-        id: req.params.id,
-      },
-    });
-    res.status(SuccessStatusCode).json({ data: SuccessDELETEMESSAGE });
-  } catch (err) {
-    res.json({ data: ERRORMESSAGE });
-  }
-});
-
-module.exports = routes;
+module.exports = router;
--- routes/permission.js
+++ routes/permission.js
@@ -1,17 +1,6 @@
-const express = require('express');
-const routes = require('express').Router();
-const { Permission } = require('../models');
+const router = require('express').Router();
+const permissionController = require('../controllers/permission.controller');
 
-routes.get('/', async (req, res) => {
-  const permission = await Permission.findAll();
-  res.status(200).json({ data: permission });
-});
-
-routes.get('/create', async (req, res) => {
-  const permission = await Permission.create({
-    name: req.body.name,
-  });
-  res.status(200).json({ data: permission });
-});
-
-module.exports = routes;
+router.get('/', permissionController.permission);
+router.post('/create', permissionController.savePermission);
+module.exports = router;
