ed02d50e4a6cab5032bb3a60f41134c44eb4e387
ismail raza <ismail.raza7292@gmail.com>
2020-12-31 13:44:56 +0500

    ts issues are resolved

--- app.ts
+++ app.ts
@@ -1,20 +1,18 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import cors from 'cors';
 import routes from './routes/index';
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 const port = process.env.PORT || 6000;
 
 app.use(cors());
 
-app.use(express.json());
 app.use('/', routes());
 
-app.use((req, res, next) => {
+app.use((_req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
   res.header(
@@ -24,9 +22,6 @@ app.use((req, res, next) => {
   res.header('Cache-Control', 'max-age=0');
   next();
 });
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(port, () => {
   console.log(`Server running on port localhost:${port}`);
--- routes/comment.routes.ts
+++ routes/comment.routes.ts
@@ -1,6 +1,5 @@
 import * as express from 'express';
 const router= express.Router();
-
 import commentController from '../controllers/comment.controller';
 
 router.post('/create', commentController.create);
